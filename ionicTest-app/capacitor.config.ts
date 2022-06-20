06c51c2f83aa9f7555b9a11d588489be5e0c1632
danijelf89@hotmail.com <danijelf89@hotmail.com>
2022-06-20 10:23:09 +0200

    Initial
    project created

---
+++ ionicTest-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicTest-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
